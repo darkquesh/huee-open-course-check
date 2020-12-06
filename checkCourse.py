9926c9b570b8142e0c6811097df7ee16b9b59153
darkquesh <erenkaradag0001@hotmail.com>
2020-12-06 23:18:22 +0300

    First commit

---
+++ checkCourse.py
@@ -0,0 +1,58 @@
+import requests
+from bs4 import BeautifulSoup
+
+def openCourses():
+	URL = 'http://www.ee.hacettepe.edu.tr/?link=300600&sublink=ugWeekly'
+	page = requests.get(URL)
+
+	soup = BeautifulSoup(page.content,'html.parser')
+	results = soup.find(id='mainPage')
+
+	open_courses = results.find_all('a')
+
+	a = []
+	for course_elem in open_courses:
+		course = course_elem.find('span')
+		if course is not None:
+			course = course.text.strip()[:6]
+			a.append(course)
+		else:
+			continue
+	return set(a)
+
+def getCourses():
+	URL = 'http://www.ee.hacettepe.edu.tr/?link=301000'
+	page = requests.get(URL)
+
+	soup = BeautifulSoup(page.content,'html.parser')
+	results = soup.find(id='mainPage')
+
+	courses = results.find_all('td',width="10%")
+
+	b = []
+	for course_elem in courses:
+		course = course_elem.find('a')
+		if course is not None:
+			course = course.text.strip()[:6]
+			b.append(course)
+		else:
+			continue
+	return set(b)
+
+openCs = sorted(openCourses())
+allCs = sorted(getCourses())
+
+res = sorted(set(allCs).intersection(openCs))
+resToStr = ' '.join(map(str,res))
+print("Open courses this term:",resToStr)
+
+
+#print("openCs: ",openCs)
+#print("\n\nallCs: ",allCs)
+
+
+
+'''for course in courses:
+	print(course.text.strip())'''
+
+#print(page.text)
